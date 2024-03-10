6cd0c15e7dd8de23e614871b04ac8ace57d7afd1
y4ri <32438034+y4ri@users.noreply.github.com>
2024-03-09 23:11:18 -0800

    update

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,76 +1,56 @@
 return {
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --   priority = 1000,
-  --   config = true,
-  --   opts = {
-  --     lazy = true,
-  --     terminal_colors = true, -- add neovim terminal colors
-  --     undercurl = true,
-  --     underline = true,
-  --     bold = false,
-  --     italic = {
-  --       strings = true,
-  --       emphasis = true,
-  --       comments = true,
-  --       operators = false,
-  --       folds = true,
-  --     },
-  --     strikethrough = true,
-  --     invert_selection = false,
-  --     invert_signs = false,
-  --     invert_tabline = false,
-  --     invert_intend_guides = false,
-  --     inverse = true, -- invert background for search, diffs, statuslines and errors
-  --     contrast = "hard", -- can be "hard", "soft" or empty string
-  --     overrides = {},
-  --     dim_inactive = false,
-  --     transparent_mode = false,
-  --     palette_overrides = {
-  --       dark0_hard = "#1b1b1b",
-  --       dark3 = "#3c3c3c",
-  --     },
-  --   },
-  -- },
-  --
   {
-    'rebelot/kanagawa.nvim',
+    'catppuccin/nvim',
     priority = 1000,
     opts = {
-      lazy = true,
-      name = "kanagawa",
-      compile = false,             -- enable compiling the colorscheme
-      undercurl = true,            -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true},
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,         -- do not set background color
-      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-      colors = {                   -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {},
-          all = {
-            ui = {
-                bg_gutter = "none"
-            },
-          }
-        },
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+          light = "latte",
+          dark = "mocha",
       },
-      -- overrides = function(colors) -- add/modify highlights
-      --     return {}
-      -- end,
-      theme = "dragon",              -- Load "wave" theme when 'background' option is not set
-      background = {               -- map the value of 'background' option to a theme
-          dark = "dragon",           -- try "dragon" !
-          light = "lotus"
+      transparent_background = false, -- disables setting the background color.
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" }, -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+              enabled = true,
+              indentscope_color = "",
+          },
       },
     },
     config = function(_, opts)
-      require('kanagawa').setup(opts)
-      vim.cmd('colorscheme kanagawa')
+      require('catppuccin').setup(opts)
+      vim.cmd('colorscheme catppuccin')
     end
   },
   {
