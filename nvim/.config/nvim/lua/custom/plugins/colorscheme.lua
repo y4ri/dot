a9f5ee5059e3376ccc1abd1ccf02f388dff586ac
y4ri <32438034+y4ri@users.noreply.github.com>
2024-08-17 18:45:53 +0200

    up

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -49,8 +49,51 @@ return {
       },
     },
     config = function(_, opts)
-      require('catppuccin').setup(opts)
-      vim.cmd('colorscheme catppuccin')
+      -- require('catppuccin').setup(opts)
+      -- vim.cmd('colorscheme catppuccin')
+    end
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    opts = {
+      contrast = "hard", -- can be "soft", "medium" or "hard"
+      palette_overrides = {
+        dark0_hard = "#1A1A1A",  -- Background color
+        dark0 = "#1A1A1A",       -- Consistent background
+        dark1 = "#282828",       -- Slightly lighter background
+        dark2 = "#3c3836",       -- UI elements and borders
+        dark3 = "#504945",       -- Black color
+        dark4 = "#665c54",       -- Selection background
+        
+        light0 = "#EBDBB2",      -- Foreground text
+        light1 = "#d5c4a1",
+        light2 = "#d5c4a1",
+        light3 = "#bdae93",
+        light4 = "#A89984",     -- White color
+
+        bright_red = "#FB4934",  -- Bright red
+        bright_green = "#B8BB26",-- Bright green
+        bright_yellow = "#FABD2F",-- Bright yellow
+        bright_blue = "#83A598", -- Bright blue
+        bright_purple = "#D3869B",-- Bright purple
+        bright_aqua = "#8EC07C", -- Bright cyan
+        
+        neutral_red = "#CC241D", -- Red
+        neutral_green = "#98971A",-- Green
+        neutral_yellow = "#D79921",-- Yellow
+        neutral_blue = "#458588",-- Blue
+        neutral_purple = "#B16286",-- Purple
+        neutral_aqua = "#689D6A",-- Cyan
+        
+        gray = "#928374",       -- Bright black
+      },
+      overrides = {},
+      transparent_mode = false,
+    },
+    config = function(_, opts)
+      require('gruvbox').setup(opts)
+      vim.cmd('colorscheme gruvbox')
     end
   },
   {
